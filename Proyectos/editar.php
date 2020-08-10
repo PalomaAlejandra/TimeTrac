d524ee70d4ec1ad1e794b50b9e020630980be5f6
PalomaAlejandra <00292467@red.unid.mx>
2020-08-10 14:55:11 -0500

    insertar,eliminar  y editar se refleja en logs

--- Proyectos/editar.php
+++ Proyectos/editar.php
@@ -1,11 +1,16 @@
 <?php
     require_once '../conexion.php';
+    session_start();
+    echo session_id();
+
     $id = $_POST['id'];
     $nombre = $_POST['nombre'];
   
 
     $consulta = "UPDATE proyectos SET nombre_proyecto = '$nombre' WHERE id_proyecto ='$id' ";
     mysqli_query($mysqli, $consulta);
+    $consulta2 = "INSERT INTO logs (accion_log, descripcion_log, fuente_log, responsable_log) VALUES ('UPDATE','Se ha actualizado un registro en la tabla','Proyectos','".$_SESSION['id']."')";
+    mysqli_query($mysqli, $consulta2);
     header("Location: index.php");
 
 
--- Usuarios/insertar.php
+++ Usuarios/insertar.php
@@ -1,12 +1,20 @@
 <?php
     require_once '../conexion.php';
+    session_start();
+    echo session_id();
+
     $nombre =$_POST['nombre'];
     $correo =$_POST['correo'];
     $password =$_POST['password'];
     $rol =$_POST['rol'];
+   
+
+    
 
     $consulta ="INSERT INTO usuarios (nombre_usr,correo_usr,password_usr,roles_id_rol) VALUES ('$nombre','$correo','$password','$rol')";
     mysqli_query($mysqli,$consulta);
+    $consulta2 = "INSERT INTO logs (accion_log, descripcion_log, fuente_log, responsable_log) VALUES ('INSERT','Se ha agregado un registro en la tabla','usuarios','".$_SESSION['id']."')";
+    mysqli_query($mysqli, $consulta2);
     header("Location: index.php")  
 
 
